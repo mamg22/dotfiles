b82e89b9c15991898677a6b4ee98136cafa5b758
mamg22 <45301823+mamg22@users.noreply.github.com>
2020-11-02 14:39:59 -0400

    Many small changes and cleanup

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,323 +1,41 @@
 # Iceberg colorscheme
 
-col = {
-    'black': '#161821',
-    'darkgray': '#6b7089',
-    'red': '#e98989',
-    'green': '#c0ca8e',
-    'yellow': '#e9b189',
-    'blue': '#91acd1',
-    'magenta': '#ada0d3',
-    'cyan': '#95c4ce',
-    'white': '#d2d4de',
+c.aliases = {
+    'w': 'session-save',
+    'q': 'quit',
+    'wq': 'quit --save',
 }
-
-c.aliases = {'w': 'session-save', 'q': 'quit', 'wq': 'quit --save'}
-
-#c.auto_save.interval
-#c.auto_save.session
 
 c.backend = 'webengine'
 
-#c.bindings.commands
-#c.bindings.default
-#c.bindings.key_mappings
-
-c.colors.completion.category.bg = '#6b7089'
-c.colors.completion.category.border.bottom = '#6b7089'
-c.colors.completion.category.border.top = '#6b7089'
-c.colors.completion.category.fg = '#d2d4de'
-c.colors.completion.even.bg = '#161821'
-c.colors.completion.fg = '#d2d4de'
-c.colors.completion.item.selected.bg = '#d2d4de'
-c.colors.completion.item.selected.border.bottom = '#d2d4de'
-c.colors.completion.item.selected.border.top = '#d2d4de'
-c.colors.completion.item.selected.fg = '#161821'
-c.colors.completion.item.selected.match.fg = '#6b7089'
-c.colors.completion.match.fg = '#6b7089'
-c.colors.completion.odd.bg = '#161821'
-c.colors.completion.scrollbar.bg = '#161821'
-c.colors.completion.scrollbar.fg = '#d2d4de'
-#c.colors.contextmenu.disabled.bg
-#c.colors.contextmenu.disabled.fg
-#c.colors.contextmenu.menu.bg
-#c.colors.contextmenu.menu.fg
-#c.colors.contextmenu.selected.bg
-#c.colors.contextmenu.selected.fg
-c.colors.downloads.bar.bg = '#161821'
-c.colors.downloads.error.bg = '#e98989'
-c.colors.downloads.error.fg = '#161821'
-c.colors.downloads.start.bg = '#84a0c6'
-c.colors.downloads.start.fg = '#161821'
-c.colors.downloads.stop.bg = '#c0ca8e'
-c.colors.downloads.stop.fg = '#161821'
-c.colors.downloads.system.bg = 'rgb'
-c.colors.downloads.system.fg = 'rgb'
-c.colors.hints.bg = 'rgba(210,212,222,.9)'
-c.colors.hints.fg = '#161821'
-c.colors.hints.match.fg = '#6b7089'
-c.colors.keyhint.bg = 'rgba(22, 24, 33, 90%)'
-c.colors.keyhint.fg = '#d2d4de'
-c.colors.keyhint.suffix.fg = '#95c4ce'
-c.colors.messages.error.bg = '#161821'
-c.colors.messages.error.border = '#161821'
-c.colors.messages.error.fg = '#e27878'
-c.colors.messages.info.bg = '#161821'
-c.colors.messages.info.border = '#161821'
-c.colors.messages.info.fg = '#d2d4de'
-c.colors.messages.warning.bg = '#161821'
-c.colors.messages.warning.border = '#161821'
-c.colors.messages.warning.fg = '#e9b189'
-c.colors.prompts.bg = '#161821'
-c.colors.prompts.border = '1px solid #6b7089'
-c.colors.prompts.fg = '#d2d4de'
-c.colors.prompts.selected.bg = '#6b7089'
-c.colors.statusbar.caret.bg = '#161821'
-c.colors.statusbar.caret.fg = '#6b7089'
-c.colors.statusbar.caret.selection.bg = '#161821'
-c.colors.statusbar.caret.selection.fg = '#c0ca8e'
-c.colors.statusbar.command.bg = '#161821'
-c.colors.statusbar.command.fg = '#d2d4de'
-c.colors.statusbar.command.private.bg = '#161821'
-c.colors.statusbar.command.private.fg = '#e9b189'
-c.colors.statusbar.insert.bg = '#161821'
-c.colors.statusbar.insert.fg = '#91acd1'
-c.colors.statusbar.normal.bg = '#161821'
-c.colors.statusbar.normal.fg = '#d2d4de'
-c.colors.statusbar.passthrough.bg = '#161821'
-c.colors.statusbar.passthrough.fg = '#ada0d3'
-c.colors.statusbar.private.bg = '#161821'
-c.colors.statusbar.private.fg = '#e9b189'
-c.colors.statusbar.progress.bg = '#d2d4de'
-c.colors.statusbar.url.error.fg = '#e27878'
-c.colors.statusbar.url.fg = '#d2d4de'
-c.colors.statusbar.url.hover.fg = '#95c4ce'
-c.colors.statusbar.url.success.http.fg = '#d2d4de'
-c.colors.statusbar.url.success.https.fg = '#b4be82'
-c.colors.statusbar.url.warn.fg = '#e2a478'
-c.colors.tabs.bar.bg = '#161821'
-c.colors.tabs.even.bg = '#161821'
-c.colors.tabs.even.fg = '#d2d4de'
-c.colors.tabs.indicator.error = '#e98989'
-c.colors.tabs.indicator.start = '#161821'
-c.colors.tabs.indicator.stop = '#d2d4de'
-c.colors.tabs.indicator.system = 'rgb'
-c.colors.tabs.odd.bg = '#161821'
-c.colors.tabs.odd.fg = '#d2d4de'
-c.colors.tabs.pinned.even.bg = '#d2d4de'
-c.colors.tabs.pinned.even.fg = '#161821'
-c.colors.tabs.pinned.odd.bg = '#d2d4de'
-c.colors.tabs.pinned.odd.fg = '#161821'
-c.colors.tabs.pinned.selected.even.bg = '#6b7089'
-c.colors.tabs.pinned.selected.even.fg = '#d2d4de'
-c.colors.tabs.pinned.selected.odd.bg = '#6b7089'
-c.colors.tabs.pinned.selected.odd.fg = '#d2d4de'
-c.colors.tabs.selected.even.bg = '#6b7089'
-c.colors.tabs.selected.even.fg = '#d2d4de'
-c.colors.tabs.selected.odd.bg = '#6b7089'
-c.colors.tabs.selected.odd.fg = '#d2d4de'
-c.colors.webpage.bg = '#ffffff'
-#c.colors.webpage.darkmode.algorithm
-#c.colors.webpage.darkmode.contrast
-#c.colors.webpage.darkmode.enabled
-#c.colors.webpage.darkmode.grayscale.all
-#c.colors.webpage.darkmode.grayscale.images
-#c.colors.webpage.darkmode.policy.images
-#c.colors.webpage.darkmode.policy.page
-#c.colors.webpage.darkmode.threshold.background
-#c.colors.webpage.darkmode.threshold.text
-#c.colors.webpage.prefers_color_scheme_dark
-
-#c.completion.cmd_history_max_items
-#c.completion.delay
-#c.completion.height
-#c.completion.min_chars
-#c.completion.open_categories
-#c.completion.quick
-#c.completion.scrollbar.padding
-#c.completion.scrollbar.width
-#c.completion.show
-#c.completion.shrink
-#c.completion.timestamp_format
-#c.completion.use_best_match
-#c.completion.web_history.exclude
-#c.completion.web_history.max_items
+c.completion.delay = 1000
 
 c.confirm_quit = ["multiple-tabs", "downloads"]
 
-#c.content.autoplay
-#c.content.cache.appcache
-#c.content.cache.maximum_pages
-#c.content.cache.size
-#c.content.canvas_reading
-#c.content.cookies.accept
-#c.content.cookies.store
-#c.content.default_encoding
-#c.content.desktop_capture
-#c.content.dns_prefetch
-#c.content.frame_flattening
-#c.content.fullscreen.overlay_timeout
-#c.content.fullscreen.window
-#c.content.geolocation
 c.content.headers.accept_language = 'es-VE,es'
-#c.content.headers.custom
-#c.content.headers.do_not_track
-#c.content.headers.referer
-#c.content.headers.user_agent
-#c.content.host_blocking.enabled
-#c.content.host_blocking.lists
-#c.content.host_blocking.whitelist
-#c.content.hyperlink_auditing
-#c.content.images
-#c.content.javascript.alert
-#c.content.javascript.can_access_clipboard
-#c.content.javascript.can_close_tabs
-#c.content.javascript.can_open_tabs_automatically
-#c.content.javascript.enabled
-#c.content.javascript.log
-#c.content.javascript.modal_dialog
-#c.content.javascript.prompt
-#c.content.local_content_can_access_file_urls
-#c.content.local_content_can_access_remote_urls
-#c.content.local_storage
-#c.content.media_capture
-#c.content.mouse_lock
-#c.content.mute
-#c.content.netrc_file
-#c.content.notifications
-#c.content.persistent_storage
-#c.content.pdfjs
-#c.content.plugins
-#c.content.print_element_backgrounds
-#c.content.private_browsing
-#c.content.proxy
-#c.content.proxy_dns_requests
-#c.content.register_protocol_handler
-#c.content.site_specific_quirks
-#c.content.ssl_strict
-#c.content.unknown_url_scheme_policy
-#c.content.user_stylesheets
-#c.content.webgl
-#c.content.webrtc_ip_handling_policy
-#c.content.xss_auditing
-
-#c.downloads.location.directory
-#c.downloads.location.prompt
-#c.downloads.location.remember
-#c.downloads.location.suggestion
-#c.downloads.open_dispatcher
-#c.downloads.position
-#c.downloads.remove_finished
 
 c.editor.command = ['st', '-e', 'nvim', '{file}', '-c', 'normal {line}G{column0}l']
-#c.editor.encoding
 
-c.fonts.completion.category = 'bold 9pt monospace'
-c.fonts.completion.entry = '9pt monospace'
-#c.fonts.contextmenu
-#c.fonts.debug_console
-#c.fonts.default_family
-#c.fonts.default_size
-c.fonts.downloads = '9pt monospace'
-c.fonts.hints = 'bold 9pt monospace'
-c.fonts.keyhint = '9pt monospace'
-c.fonts.messages.error = '9pt monospace'
-c.fonts.messages.info = '9pt monospace'
-c.fonts.messages.warning = '9pt monospace'
-#c.fonts.prompts
-c.fonts.statusbar = '9pt monospace'
-c.fonts.tabs.selected = '9pt monospace'
-c.fonts.tabs.unselected = '9pt monospace'
-#c.fonts.web.family.cursive
-#c.fonts.web.family.fantasy
-#c.fonts.web.family.fixed
+c.fonts.default_size = '9pt'
 c.fonts.web.family.sans_serif = 'Liberation Sans'
-#c.fonts.web.family.serif
 c.fonts.web.family.standard = 'Liberation Sans'
 c.fonts.web.size.default = 15
-#c.fonts.web.size.default_fixed
-#c.fonts.web.size.minimum
-#c.fonts.web.size.minimum_logical
 
-#c.hints.auto_follow
-#c.hints.auto_follow_timeout
 c.hints.border = '1px solid #6b7089'
 c.hints.chars = 'asdfghjkl'
-#c.hints.dictionary
-#c.hints.find_implementation
-#c.hints.hide_unmatched_rapid_hints
-#c.hints.leave_on_load
-#c.hints.min_chars
 c.hints.mode = 'letter'
-#c.hints.next_regexes
-#c.hints.padding
-#c.hints.prev_regexes
-#c.hints.radius
-#c.hints.scatter
-#c.hints.selectors
 c.hints.uppercase = True
 
-#c.history_gap_interval
-
-#c.input.escape_quits_reporter
-#c.input.forward_unbound_keys
-#c.input.insert_mode.auto_enter
-#c.input.insert_mode.auto_leave
-#c.input.insert_mode.auto_load
-#c.input.insert_mode.leave_on_load
-#c.input.insert_mode.plugins
-#c.input.links_included_in_focus_chain
-#c.input.mouse.back_forward_buttons
-#c.input.mouse.rocker_gestures
-#c.input.partial_timeout
-#c.input.spatial_navigation
-
-#c.keyhint.blacklist
-#c.keyhint.delay
-#c.keyhint.radius
-
-#c.logging.level.console
-#c.logging.level.ram
-
-#c.messages.timeout
-
-#c.new_instance_open_target
-#c.new_instance_open_target_window
-
-#c.prompt.filebrowser
-#c.prompt.radius
-
-#c.qt.args
-#c.qt.force_platform
-#c.qt.force_platformtheme
-#c.qt.force_software_rendering
-#c.qt.highdpi
 c.qt.low_end_device_mode = 'always'
-#c.qt.process_model
 
-#c.scrolling.bar
 c.scrolling.smooth = False
 
-#c.search.ignore_case
-#c.search.incremental
-#c.search.wrap
-
-#c.session.default_name
 c.session.lazy_restore = True
 
-#c.spellcheck.languages
-
-#c.statusbar.padding
 c.statusbar.position = 'bottom'
 c.statusbar.show = 'always'
-#c.statusbar.widgets
 
 c.tabs.background = True
-#c.tabs.close_mouse_button
-#c.tabs.close_mouse_button_on_bar
-#c.tabs.favicons.scale
-#c.tabs.favicons.show
-#c.tabs.focus_stack_size
 c.tabs.indicator.padding = {
     'top': 2,
     'bottom': 2,
@@ -326,38 +44,20 @@ c.tabs.indicator.padding = {
 }
 c.tabs.indicator.width = 2
 c.tabs.last_close = 'default-page'
-#c.tabs.max_width
-#c.tabs.min_width
-#c.tabs.mode_on_change
 c.tabs.mousewheel_switching = False
-#c.tabs.new_position.related
-#c.tabs.new_position.stacking
-#c.tabs.new_position.unrelated
 c.tabs.padding = {
     'top': 0,
     'bottom': 0,
     'left': 4,
     'right': 4
 }
-#c.tabs.pinned.frozen
-#c.tabs.pinned.shrink
 c.tabs.position = 'top'
-#c.tabs.select_on_remove
 c.tabs.show = 'multiple'
-#c.tabs.show_switching_delay
-#c.tabs.tabs_are_windows
 c.tabs.title.alignment = 'left'
 c.tabs.title.format = '{audio}{index}: {current_title}'
-#c.tabs.title.format_pinned
-#c.tabs.tooltips
-#c.tabs.undo_stack_size
 c.tabs.width = '5%'
-#c.tabs.wrap
 
-#c.url.auto_search
 c.url.default_page = '~/any/startpage/index.html'
-#c.url.incdec_segments
-#c.url.open_base_url
 c.url.searchengines = {
     'DEFAULT': 'https://duckduckgo.com/?q={}',
     'd':       'https://duckduckgo.com/?q={}',
@@ -382,22 +82,16 @@ c.url.searchengines = {
     'ghr':     'https://github.com/{unquoted}',
 }
 c.url.start_pages = '~/any/startpage/index.html'
-#c.url.yank_ignored_parameters
 
-#c.window.hide_wayland_decoration
-#c.window.title_format
-
-#c.zoom.default
-#c.zoom.levels
-c.zoom.mouse_divider = 0 # Disable mouse wheel zoom
-#c.zoom.text_only
+# Disable mouse wheel zoom
+c.zoom.mouse_divider = 0
 
 tab_rotate = [
     'config-cycle tabs.position top right',
-    'config-cycle tabs.title.format "{audio}{index}: {current_title}" "{index}"',
+    'config-cycle tabs.title.format "{audio}{index}: {current_title}" "{aligned_index}"',
     'config-cycle tabs.title.alignment left center',
     'set tabs.position?',
- ]
+]
 
 tab_show_cycle = [
     'config-cycle tabs.show multiple switching never',
@@ -409,12 +103,11 @@ bar_show_toggle = [
     'set statusbar.show?',
 ]
 
-#user_agent_cycle = [
-#    'config-cycle content.headers.user_agent "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0" ""',
-#    'set content.headers.user_agent?',
-#]
+def join_commands(command_list):
+    return ' ;; '.join(command_list)
 
-config.bind('<F2>', ' ;; '.join(tab_rotate))
-config.bind('<F3>', ' ;; '.join(tab_show_cycle))
-config.bind('<F4>', ' ;; '.join(bar_show_toggle))
-#config.bind('<F5>', ' ;; '.join(user_agent_cycle))
+config.bind('xr', join_commands(tab_rotate))
+config.bind('xt', join_commands(tab_show_cycle))
+config.bind('xb', join_commands(bar_show_toggle))
+
+config.source("colors.py")
